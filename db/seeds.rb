fbc8cfb959ac7afa3117f4a3d651c36c1f869daf
stephane fersing <stephane.fersing@gmail.com>
2020-02-13 16:29:04 +0100

    Done for MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ingredient_1 = Ingredient.create(name: "lemon")
+ingredient_2 = Ingredient.create(name: "ice")
+ingredient_3 = Ingredient.create(name: "mint leaves")
+ingredient_4 = Ingredient.create(name: "tomato juice")
+ingredient_5 = Ingredient.create(name: "orange juice")
+ingredient_6 = Ingredient.create(name: "whisky")
+ingredient_7 = Ingredient.create(name: "vodka")
+ingredient_8 = Ingredient.create(name: "rhum")
+
+cocktail_1 = Cocktail.create(name: "lemon grass")
+cocktail_2 = Cocktail.create(name: "ice grass")
+
+
+dose_1 = Dose.create(description: "6cl", cocktail_id: 1, ingredient_id: 1)
+dose_2 = Dose.create(description: "6cl", cocktail_id: 2, ingredient_id: 2)
